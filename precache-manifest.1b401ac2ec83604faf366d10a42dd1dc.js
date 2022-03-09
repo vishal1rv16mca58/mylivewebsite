e4b913b70b3271bf0b38192ce935ec9a5d05ccec
VISHAL <vishal@VISHALs-MacBook-Air.local>
2022-03-09 12:36:06 +0530

    Updates

---
+++ precache-manifest.1b401ac2ec83604faf366d10a42dd1dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c2ef0ecd8f11bb08968",
+    "url": "/mylivewebsite/static/css/main.5a1ae9ae.chunk.css"
+  },
+  {
+    "revision": "5c2ef0ecd8f11bb08968",
+    "url": "/mylivewebsite/static/js/main.5c2ef0ec.chunk.js"
+  },
+  {
+    "revision": "50900b269cf123b3e868",
+    "url": "/mylivewebsite/static/js/1.50900b26.chunk.js"
+  },
+  {
+    "revision": "8836b42d9726ffdf442e",
+    "url": "/mylivewebsite/static/js/runtime~main.8836b42d.js"
+  },
+  {
+    "revision": "66b8c885f67c9f95cdf87f7cd76bf386",
+    "url": "/mylivewebsite/index.html"
+  }
+];
